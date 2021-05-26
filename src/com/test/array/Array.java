ba101bea6114f79f2deab46311ceb7f7bbbe4190
Mr_Long <1169347662@qq.com>
2021-05-26 11:58:25 +0800

    力扣初级算法

--- src/com/test/array/Array.java
+++ src/com/test/array/Array.java
@@ -1,6 +1,6 @@
 package com.test.array;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class Array {
     public static void main(String[] args) {
@@ -20,6 +20,32 @@ public class Array {
 //        rotate(nums, k);
 //        System.out.println(Arrays.toString(nums));
 
+        //4.存在重复元素
+//        int nums[] = {1, 2, 3, 1};
+//        System.out.println(containsDuplicate(nums));
+
+        //5.只出现一次的数字
+//        int nums[] = {2, 1, 2};
+//        System.out.println(singleNumber(nums));
+
+        //两个数组交集
+//        int nums1[] = {4,9,5};
+//        int nums2[] = {9,4,9,8,4};
+//        System.out.println(Arrays.toString(intersect(nums1,nums2)));
+
+        //整数加一
+//        int nums[] = {1, 2, 9};
+//        System.out.println(Arrays.toString(plusOne(nums)));
+
+        //移动0到最后 保持非0次序不变
+//        int nums[] = {0, 1, 6, 3, 12, 0};
+//        moveZeroes(nums);
+//        System.out.println(Arrays.toString(nums));
+
+        //两数之和(返回目标值下标)
+        int nums[] = {2, 3, 3, 15};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
     public static int removeDuplicates(int[] nums) {
@@ -68,15 +94,142 @@ public class Array {
         //2.
         k %= len;
         reverse(nums, 0, len - 1);
-        reverse(nums, 0, k-1);
+        reverse(nums, 0, k - 1);
         reverse(nums, k, len - 1);
     }
+
     public static void reverse(int[] nums, int start, int end) {
         while (start < end) {
             int temp = nums[start];
             nums[start++] = nums[end];
             nums[end--] = temp;
         }
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int singleNumber(int[] nums) {
+//        1.
+//        Arrays.sort(nums);
+//        int len = nums.length;
+//        for (int i = 0; i < len - 1; i++) {
+//            if (nums[i] == nums[i + 1]) {
+//                i++;
+//            } else {
+//                return nums[i];
+//            }
+//        }
+//        return nums[len - 1];
+        //2.
+        int reduce = 0;
+        for (int i : nums) {
+            reduce ^= i;
+        }
+        return reduce;
+    }
+
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] > nums2[j]) {
+                j++;
+            } else if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else {
+                i++;
+            }
+        }
+        int[] res = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            res[k] = list.get(k);
+        }
+        return res;
+    }
+
+    public static int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] newDigit = new int[len + 1];
+        newDigit[0] = 1;
+        return newDigit;
+    }
+
+    public static void moveZeroes(int[] nums) {
+//        int len = nums.length - 1;
+//        for (int i = len; i >= 0; i--) {
+//            if (nums[i] == 0) {
+//                reverse(nums, i, len);
+//                reverse(nums, i, len-1);
+//            }
+//        }
+
+        //2.
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+//        for (int i = 0; i < len; i++) {
+//            for (int j = i+1; j < len; j++){
+//                if (nums[i] + nums[j] == target){
+//                    return new int[]{i, j};
+//                }
+//            }
+//        }
+//        return new int[]{-1, -1};
+        //2.
+//        int i = 0, j = 1;
+//        while (nums[i] + nums[j] != target) {
+//            if (j == len - 1) {
+//                i++;
+//                j = i;
+//            }
+//            j++;
+//        }
+//        return new int[]{i, j};
+
+        //3.哈斯表 差值查找
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] index = new int[2];
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(target - nums[i])) {
+                index[0] = map.get(target - nums[i]);
+                index[1] = i;
+                break;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return index;
     }
 }
 
